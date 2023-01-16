ad63f52e7ce64ca381cc8d11cd4491f6fac6c874
Ded-Rinat <rigelz@yandex.ru>
2023-01-16 23:03:35 +0300

    Моя первый проект на Java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,53 @@
+
+import java.util.Scanner;
+
 // dev branch for Y.Practicum
-public class Main {
+public class  Main {
+
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        int countPersons;//количество человек
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На сколько человек необходимо разделить счёт?");
+
+
+        while (true) {
+
+            if (scanner.hasNextInt()) {
+                countPersons = scanner.nextInt();
+
+                if (countPersons <= 1) {
+                    System.out.println("Некорректное значение, введите число больше 1");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Ошибка,нужно ввести целочисленное число");
+
+            }
+            scanner.nextLine();
+
+        }
+
+        Products products = new Products();
+
+        products.TotalproductAndPrice();
+
+
+        double result=Products.totalPrice/countPersons;//подсчет сколько должен заплатить каждый
+
+        Convert convert=new Convert();
+
+        System.out.printf("К оплате с человека: %.2f %s ",result, Convert.convertRub(result));
+
     }
 }
+
+
+
+
+
+
+
+
